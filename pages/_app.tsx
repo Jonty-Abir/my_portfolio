f21df59c217d46ae4308ec5c0a51a48acc7c8c62
Jonty-Abir <abirrens@gmail.com>
2023-01-16 16:14:57 +0530

    2nd commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import "@/styles/dist/dist.css";
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
