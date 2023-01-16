662d46aa4aa059592e853010d413b77920514a09
Jonty-Abir <abirrens@gmail.com>
2023-01-17 02:22:54 +0530

    1st_commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" id="theme" className="scroll-smooth dark">
+    <Html lang="en" id="theme" className="dark">
       <Head />
       <body className="dark:bg-gray-900 bg-white text-gray-900">
         <Main />
